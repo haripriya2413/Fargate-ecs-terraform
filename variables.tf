de232b84947db7d684c415bb68aef345316b8b5a
haripriya2413 <143806953+haripriya2413@users.noreply.github.com>
2024-06-09 17:43:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,6 @@
 # variables.tf
 
-variable "aws_access_key" {
-    description = "The IAM public access key"
 
-}
-
-variable "aws_secret_key" {
-    description = "IAM secret access key"
-   
-}
 
 variable "aws_region" {
     description = "The AWS region things are created in"
