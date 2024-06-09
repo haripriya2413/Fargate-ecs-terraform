c2ff931312c08f4b4b81917487ec31962703f942
haripriya2413 <143806953+haripriya2413@users.noreply.github.com>
2024-06-09 17:45:16 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,6 @@
 
 # Specify the provider and access details
 provider "aws" {
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
-    region                  = var.aws_region
+ 
+    region     = var.aws_region
 }
