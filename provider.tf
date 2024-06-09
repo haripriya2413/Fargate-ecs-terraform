9f5eff5e4ba4b6f3f9df16ab81759e7e2e36ac55
haripriya2413 <143806953+haripriya2413@users.noreply.github.com>
2024-06-09 17:46:54 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,3 +5,9 @@ provider "aws" {
  
     region     = var.aws_region
 }
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-priya243523"
+    key    = "ecs-fargate/terraform.tfstate"
+    region = "us-east-1"
+  }
